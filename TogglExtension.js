bab50d3db8608eefe4bab945426f4e10bee85a20
poulfiksit <80675849+poulfiksit@users.noreply.github.com>
2021-11-19 13:50:57 +0100

    Update TogglExtension.js

--- TogglExtension.js
+++ TogglExtension.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Toggl Description Extension
+// @name         Toggl Description Plugin
 // @namespace    https://fiksit.dk/
 // @version      1.0
 // @description  Check if Descriptions are longer than 50 characters. Properly...
-// @author       Poul Bach Lauritsen @ FiksIT
+// @author       Poul Bach Lauritsen @ Fiks IT
 // @match        https://track.toggl.com/timer
 // @downloadURL  https://github.com/poulfiksit/TogglWarning/blob/main/TogglExtension.js
 // @updateURL    https://github.com/poulfiksit/TogglWarning/blob/main/TogglExtension.js
@@ -11,12 +11,17 @@
 // ==/UserScript==
 
 (function() {
-  'use strict';
 
     const targetNode = document.body
     const config = {attributes: true, childList: true, subtree: true}
     let entryList
     let topdiv
+
+    let VARS = {
+    MAX_DESCRIPTION_LENGTH: 50,
+    COLOR: "red",
+    BORDERCOLOR: "red"
+    }
 
     const callback = function(mutationsList, observer) {
 
@@ -26,22 +31,22 @@
                     let collection = document.getElementsByClassName(' css-syh4oc-TimeEntryDescription e10vqxue0')
                     topdiv = document.getElementsByClassName('css-1wbxlms-Description ea2nj4x2')[0]
                     entryList = Array.from(collection)
-                    initialize(entryList)
-                    refresh(entryList, topdiv)
+                    initialize(entryList, VARS)
+                    refresh(entryList, topdiv, VARS)
                 }
                 if(mutation.target.className == "css-oqj558-List e9ma3si0") {
                     let collection = document.getElementsByClassName(' css-syh4oc-TimeEntryDescription e10vqxue0')
                     entryList = Array.from(collection)
-                    refresh(entryList, topdiv)
+                    refresh(entryList, topdiv, VARS)
                 }
                 if(mutation.target.className == "css-yht0st-Root et8m3zx1") {
                     let collection = document.getElementsByClassName(' css-syh4oc-TimeEntryDescription e10vqxue0')
                     entryList = Array.from(collection)
-                    refresh(entryList, topdiv)
+                    refresh(entryList, topdiv, VARS)
                 }
                 if(mutation.target.className.includes("e1ln5t0v1")) {
                     let shitput = document.getElementsByClassName("e1ln5t0v1 css-ep8ani-Input-InputCss-FluidTextInputStyled ewv0efw0")[0]
-                   refresh(entryList, topdiv, shitput)
+                   refresh(entryList, topdiv, VARS, shitput)
                 }
             }
         }
@@ -58,21 +63,21 @@
       if(event.target.className == "e1ln5t0v1 css-ep8ani-Input-InputCss-FluidTextInputStyled ewv0efw0")
       {
           let shitput = document.getElementsByClassName("e1ln5t0v1 css-ep8ani-Input-InputCss-FluidTextInputStyled ewv0efw0")[0]
-          refresh(entryList, topdiv, shitput)
+          refresh(entryList, topdiv, VARS, shitput)
       } else {
-          refresh(entryList, topdiv)
+          refresh(entryList, topdiv, VARS)
       }
 
   })
     document.body.addEventListener('onclick', (event) => {
 
         if(event.target.className == "css-1xmqmav-TimerButton-button ew4ipl50") {
-                        refresh(entryList, topdiv)
+                        refresh(entryList, topdiv, VARS)
         }
     })
 })();
 
-function initialize(entryList) {
+function initialize(entryList, VARS) {
 
      for(let i = 0; i < entryList.length; i++) {
       let lol = null
@@ -83,22 +88,23 @@ function initialize(entryList) {
       }
 
       if(lol != null) {
-      createCharCounter(lol)
+      createCharCounter(lol, VARS)
       }
     }
     if(document.getElementsByClassName("css-1a08mon-TimerFormContent e191y2gl1")[0]) {
-      createCharCounter(document.getElementsByClassName("css-1a08mon-TimerFormContent e191y2gl1")[0], true)
+      createCharCounter(document.getElementsByClassName("css-1a08mon-TimerFormContent e191y2gl1")[0], VARS, true)
     }
 }
 
-function refresh(entryList, topdiv, shitput = null) {
-    updateCharCounter(topdiv.parentNode.parentNode, topdiv.innerHTML.length, true)
-    if(topdiv.innerHTML.length > 50) {
-         topdiv.style.border = '2px solid red'
-         topdiv.style.setProperty('color', 'red', 'important')
+function refresh(entryList, topdiv, VARS, shitput = null) {
+
+    updateCharCounter(topdiv.parentNode.parentNode, topdiv.innerHTML.length, VARS, true)
+    if(topdiv.innerHTML.length > VARS.MAX_DESCRIPTION_LENGTH) {
+         topdiv.style.border = '2px solid ' + VARS.BORDERCOLOR
+         topdiv.style.setProperty('color', VARS.COLOR, 'important')
          if(shitput != null) {
-         shitput.style.border = '2px solid red'
-         shitput.style.setProperty('color', 'red', 'important')
+         shitput.style.border = '2px solid ' + VARS.BORDERCOLOR
+         shitput.style.setProperty('color', VARS.COLOR, 'important')
          }
     } else {
           topdiv.style.border = 'none'
@@ -109,10 +115,10 @@ function refresh(entryList, topdiv, shitput = null) {
     }
 
      for(let i = 0; i < entryList.length; i++) {
-      updateCharCounter(entryList[i], entryList[i].innerHTML.length)
-      if(entryList[i].innerHTML.length > 50) {
-         entryList[i].style.border = '2px solid red'
-         entryList[i].style.setProperty('color', 'red', 'important')
+      updateCharCounter(entryList[i], entryList[i].innerHTML.length, VARS)
+      if(entryList[i].innerHTML.length > VARS.MAX_DESCRIPTION_LENGTH) {
+         entryList[i].style.border = '2px solid ' + VARS.BORDERCOLOR
+         entryList[i].style.setProperty('color', VARS.COLOR, 'important')
       } else {
           entryList[i].style.border = 'none'
           entryList[i].style.color = ''
@@ -120,7 +126,8 @@ function refresh(entryList, topdiv, shitput = null) {
     }
 }
 
-function updateCharCounter(entry, length, top = false) {
+function updateCharCounter(entry, length, VARS, top = false) {
+
       let lol = null
 
       if(!top) {
@@ -134,10 +141,10 @@ function updateCharCounter(entry, length, top = false) {
       }
       if(lol != null) {
 
-          lol.innerHTML = length + '/50'
-          if(length > 50) {
+          lol.innerHTML = length + '/' + VARS.MAX_DESCRIPTION_LENGTH.toString()
+          if(length > VARS.MAX_DESCRIPTION_LENGTH) {
 
-              lol.style.setProperty('color', 'red', 'important')
+              lol.style.setProperty('color', VARS.COLOR, 'important')
           }
           else {
           lol.style.color = ''
@@ -145,19 +152,16 @@ function updateCharCounter(entry, length, top = false) {
       }
 }
 
-function createCharCounter(div, top = false) {
+function createCharCounter(div, VARS, top = false) {
 
             const charCounterCurrent = document.createElement('div')
-            charCounterCurrent.innerHTML = '0/50'
+            charCounterCurrent.innerHTML = '0/' + VARS.MAX_DESCRIPTION_LENGTH.toString()
             charCounterCurrent.style.padding = '10px'
             charCounterCurrent.style.textAlign = 'right'
             charCounterCurrent.style.color = 'darkgrey'
             charCounterCurrent.id = 'inputLengthField'
             charCounterCurrent.className = 'funkyFields'
 
-
-
-    //div.insertBefore(charCounterCurrent, div.getElementsByClassName("css-718bxe-Root-Root-Root e1jfstgf0")[0])
     if(div.getElementsByClassName("funkyFields").length == 0 && !top) {
         div.insertBefore(charCounterCurrent, div.getElementsByClassName("css-ah4f6o-TriggerRoot e1jfstgf1")[0])
     }
